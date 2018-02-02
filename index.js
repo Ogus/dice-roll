5515ca7e18b29545a3d892f511d1b9eda0070748
Ogus <ogustin.gagnon@gmail.com>
2018-02-02 11:24:20 +0100

    New test

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 'use strict';
 
-function randInt(a,b) {
-  return a + Math.floor(Math.random()*(b-a));
+var DEFAULT_VALUE = 6;
+
+function random(a,b) {
+  return a + Math.floor(Math.random()*(b+1-a));
 }
 
 function extractParameters(n, s) {
   var type_n = typeof(n);
   var type_s = typeof(s);
-  var a, b;
+  var a = undefined, b = undefined;
   var defined = false;
 
-  // parameters: ("1d6")
-  if(!defined && type_n == "string" && type_s == "undefined") {
-    var temp = n.toUpperString().split("D");
-    a = parseInt(temp[0]);
-    b = parseInt(temp[1]);
+  if(!defined && (type_n == "string" || type_n == "number") && type_s == "undefined") {
+    // parameters: ("2d6")
+    var temp = String(n).toUpperString().split("D");
+    if(temp.lengh == 2) {
+      a = parseInt(temp[0]);
+      b = parseInt(temp[1]);
 
-    if( !(isNaN(a) || isNaN(b)) ) {
-      defined = true;
+      if( !(isNaN(a) || isNaN(b)) ) {
+        defined = true;
+      }
+    }
+
+    // parameters: ("2") or (2)
+    else {
+      a = parseInt(n);
+      if(!isNaN(a)) {
+        b = DEFAULT_VALUE;
+        defined = true;
+      }
     }
   }
 
-  // parameters: ("1", "6") or ("1", 6) or (1, 6) ...
+  // parameters: ("2", "6") or ("2", 6) or (2, "6") or (2, 6)
   if(!defined && (type_n == "string" || type_n == "number") && (type_s == "string" || type_s == "number")) {
     a = parseInt(n);
     b = parseInt(s);
@@ -34,24 +47,25 @@ function extractParameters(n, s) {
   // set to default
   if(!defined) {
     a = 1;
-    b = 6;
+    b = DEFAULT_VALUE;
   }
 
   return [a, b];
 }
 
 
-function roll (n,s) {
+function roll (n, s) {
   var [a, b] = extractParameters(n, s);
 
   var r = 0;
   for (var i = 0; i < a; i++) {
-    r += randInt(1, b+1);
+    r += random(1, b);
   }
   return r;
 };
 
 
 module.exports = {
-  roll: roll
+  roll: roll,
+  random: random
 };
--- test/test.js
+++ test/test.js
@@ -4,10 +4,15 @@ var dice = require('../index');
 describe('Dice', function() {
 
   describe('basic roll', function() {
-    it('should return a value in range [0,5]', function() {
+    it('return a value in range [0,5]', function() {
       var r = dice.roll();
-      assert.ok(r > 0, "Value below 1: "+String(r) );
-      assert.ok(r < 7, "Value above 6: "+String(r) );
+      assert.ok(r > 0, "Value below 1: "+String(r));
+      assert.ok(r < 7, "Value above 6: "+String(r));
+    });
+
+    it('is an integer', function () {
+      var r = dice.roll();
+      assert.equal(r, Math.floor(r), "Not an integer: "+String(r));
     });
   });
 
